3e762cae31c7dc1e5ef072fe7e8ddeab281bf2ea
Akshay Jangid <akshay.dce@gmail.com>
2018-03-03 18:11:41 -0800

    updated tokens

--- aws/sts/invoke-lambda.py
+++ aws/sts/invoke-lambda.py
@@ -6,14 +6,14 @@ url = "https://lambda.us-east-1.amazonaws.com/2015-03-31/functions/scoring/invoc
 
 payload = "{\"x\": 6,\"y\": 9}"
 headers = {
-    'x-amz-date': "20180304T011932Z",
-    'authorization': "AWS4-HMAC-SHA256 Credential=ASIAJ6APQ544BWZFEKBA/20180304/us-east-1/lambda/aws4_request, SignedHeaders=content-length;content-type;host;x-amz-date;x-amz-security-token, Signature=c0413790fc76e665f8d18373dd776f22e0c39c73cdd57741d41ea381a7e1f8d4",
-    'x-amz-security-token': "FQoDYXdzEDoaDMu+1KS4LXeCmWo+MiLnAXmxNWFVxoHYuARJbBoEqZsw9YxMaaJ4DOEV6x53dmiu/7B8I067AGpOzLPJ6sDlvLAYd/OBc8DDGnNTdliB/DUno82OQeAcpuEt4YUB3TOEYFfa//6WWPqPt7nWOqPW6WybTo0ihJxkzRIj59zj/Amwy0totd8fD9QYFWfDsOKFMNJwHDnhO3dfHmlUIw3rexamZywYB68987TpDyglgu4XXUf3J7cQ/Ge+xnm7B1gRvX53uPPQ5Mmxjl78nPcVCZaHXQRyFH7sg6PqacE2yjg6666ewSCnMnP7xETmZB6i8rVvzf7USCiFke3UBQ==",
+    'x-amz-date': "20180304T020831Z",
+    'authorization': "AWS4-HMAC-SHA256 Credential=ASIAJ25I7AD67LWW66KQ/20180304/us-east-1/lambda/aws4_request, SignedHeaders=content-length;content-type;host;x-amz-date;x-amz-security-token, Signature=e772f9660517d0bb81b7497f07112640372e28aab8f75690f1aa14647ab6ea51",
+    'x-amz-security-token': "FQoDYXdzEDsaDPJO2VN1fyF32SgRSyLnAbvmsE9OtU5X/tFnKW9AIQTA8Pic0cRWO0GmyzrJ/JCuUWfGuawc9xamCui8ngYjdt2tm9hVlnSzB9nU0ZyGYBV9nf0vAi4pblIrQ+BHKWoPoWFdpgMFjnuCCVIEri6a7LdhSebA1Sdt9qhD8y9ecQncixaC9SNZDXo4NBPHrhpFnvnUJenEgvjRoDYwEG3Mp/YCDekYLQy8VcQPhAUdrWcjJ636yqKznjBwPCBJFA55tmZtJeDffck3yYSblD+ierFXy6M9Nz5qjxDAra87DtwBYbFbtCDBVRkbiBr6FkoNrL7VdTcAJCjPpe3UBQ==",
     'content-type': "application/json",
     'host': "lambda.us-east-1.amazonaws.com",
     'cache-control': "no-cache",
-    'postman-token': "9f541fff-0daf-fbd1-384f-169f000bb0b6"
-    }
+    'postman-token': "c95d2e3a-904f-1c6b-c1df-f6c21f889d16"
+}
 
 response = requests.request("POST", url, data=payload, headers=headers)
 
